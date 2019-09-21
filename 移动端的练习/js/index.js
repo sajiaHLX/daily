6c79a36ea91549ba7aecbfbd0ce0219db912d406
sajiaHLX <690906573@qq.com>
2019-09-21 21:21:37 +0800

    2019.9.21

--- 移动端的练习/js/index.js
+++ 移动端的练习/js/index.js
@@ -137,7 +137,7 @@ let messageRender = (function () {
         autoTimer = null,
         interval = 1500; //记录信息出来的间隔时间
     //展示信息
-    let tt=0;
+    let tt = 0;
     let showMessage = function showMessage() {
         ++step;
         if (step === 2) {
@@ -190,7 +190,7 @@ let messageRender = (function () {
             <img src="img/zf_messageStudent.png" alt="" class="pic">
             ${$textInp.html()}
         </li>`).insertAfter($messageList.eq(1)).addClass('active');
-        $messageList = $wrapper.find('li');//=>重要:把新的LI放到页面中,我们此时应该重新获取LI，让MESSAGE-LIST和页面中的LI正对应，方便后期根据索引展示对应的LI
+        $messageList = $wrapper.find('li'); //=>重要:把新的LI放到页面中,我们此时应该重新获取LI，让MESSAGE-LIST和页面中的LI正对应，方便后期根据索引展示对应的LI
 
         //=>该消失的消失
         $textInp.html('');
@@ -228,6 +228,103 @@ let messageRender = (function () {
         }
     }
 })();
+
+/* cube */
+let cubeRender = (function () {
+    let $cubeBox = $('.cubeBox'),
+        $cube = $('.cube'),
+        $cubeList = $cube.find('li');
+
+    //手指控制旋转
+    let start = function start(ev) {
+        //记录手指按住时的起始坐标位置
+        let point = ev.changedTouches[0];
+        this.strX = point.clientX;
+        this.strY = point.clientY;
+        this.changeX = 0;
+        this.changeY = 0;
+    }
+    let move = function move(ev) {
+        //用最新的手指位置-起始的位置，记录x和y轴的偏移量
+        let point = ev.changedTouches[0];
+        this.changeX = point.clientX - this.strX;
+        this.changeY = point.clientY - this.strY;
+    }
+    let end = function end(ev) {
+        let {
+            changeX,
+            changeY,
+            rotateX,
+            rotateY
+        } = this,
+        isMove = false;
+        //验证是否发生移动（判断滑动误差）
+        Math.abs(changeX) > 10 || Math.abs(changeY) > 10 ? isMove = true : null;
+        //只有发生移动是才处理
+        if (isMove) {
+            //1.左右滑=>CHANGE-X=>ROTATE-Y (正比:CHANGE越大ROTATE越大)
+            //2.上下滑=>CHANGE-Y=>ROTATE-X (反比:CHANGE越大ROTATE越小)
+            //3.为了让每一次操作旋转角度小一点，我们可以把移动距离的1/3作为旋转的角度即可
+            rotateX = rotateX - changeY / 3;
+            rotateY = rotateY + changeX / 3;
+            //=>赋值给魔方盒子
+            $(this).css('transform', `scale(0.6) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`);
+            //=>让当前旋转的角度成为下一次起始的角度
+            this.rotateX = rotateX;
+            this.rotateY = rotateY;
+        }
+        ['strX', 'strY', 'changeX', 'changeY'].forEach(item => this[item] = null);
+    }
+    return {
+        init: function () {
+            $cubeBox.css('display', 'block');
+
+            //手指操作cube，让cube跟着旋转
+            let cube = $cube[0];
+            cube.rotateX = -35;
+            cube.rotateY = 35; //记录初始的旋转角度【存储到自定义属性】
+            $cube.on('touchstart', start).on('touchmove', move).on('touchend', end);
+        }
+    }
+})();
+
+/* detail */
+let detailRender = (function () {
+    let $detailBox = $('.detailBox'),
+        swiper = null;
+    let swiperInit = function swiperInit() {
+        swiper = new Swiper('.swiper-container', {
+            //这里放你需要实现的效果
+            effect: "coverflow",
+            // loop:true
+            // freeMode: true,
+            // slidesPerView: 'auto',
+            observer: true, //修改swiper自己或子元素时，自动初始化swiper
+            observeParents: true, //修改swiper的父元素时，自动初始化swiper
+            onInit:(swiper)=>{
+                //切换动画成功执行的回调函数（参数是当前初始化的实例）
+            },
+            onTransitonEnd:(swiper)=>{
+                //切换动画完成执行的回调函数
+            }
+        });
+        /* 
+         *实例的私有属性：
+         *1.activeIndex：当前展示的slide块的索引
+         *2.slides：获取所有的slide（数组）
+         *....
+         *实例的公有方法
+         *1.slideTo：切换到指定索引的slide
+         *....
+        */
+    }
+    return {
+        init: function () {
+            $detailBox.css('display', 'block');
+            swiperInit();
+        }
+    }
+})();
 /* 开发过程中，由于当前项目板块众多（每一个板块都是单例），我们最好规划一种机制：通过标识的判
  *断可以让程序只执行对应板块的内容，这样开发哪一个板块，我们就把标识改为啥（hash路由控制）
  */
@@ -243,6 +340,12 @@ switch (hash) {
         break;
     case 'message':
         messageRender.init();
+        break;
+    case 'cube':
+        cubeRender.init();
+        break;
+    case 'detail':
+        detailRender.init();
         break;
     default:
         loadingRender.init();
