d5890f33fb9121bc45d9b55a18b07550d2430042
sajiaHLX <690906573@qq.com>
2019-11-09 16:35:51 +0800

    2019.11.9

--- VueStudy/axiosstudy/src/network/request.js
+++ VueStudy/axiosstudy/src/network/request.js
@@ -1,14 +1,23 @@
 import axios from 'axios'
 export function request1(config) {
-  return new Promise((resolve, reject) => {
     const instance1 = axios.create({
       baseURL: 'http://127.0.0.1:3000',
       timeout: 5000
     })
-    instance1(config).then(res => {
-      resolve(res)
-    }).catch(err => {
-      reject(err)
-    })
-  })
+    return instance1(config)
 }
+
+
+// export function request1(config) {
+//   return new Promise((resolve, reject) => {
+//     const instance1 = axios.create({
+//       baseURL: 'http://127.0.0.1:3000',
+//       timeout: 5000
+//     })
+//     instance1(config).then(res => {
+//       resolve(res)
+//     }).catch(err => {
+//       reject(err)
+//     })
+//   })
+// }
